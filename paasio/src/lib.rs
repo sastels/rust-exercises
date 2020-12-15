04b4e8670f740eb1a3806ab1e93ddf9eaa8f8d66
Steve Astels <sastels@gmail.com>
2020-12-15 12:42:10 -0500

    mash keyboard some more

--- paasio/src/lib.rs
+++ paasio/src/lib.rs
@@ -43,35 +43,47 @@ impl<R: Read> Read for ReadStats<R> {
     }
 }
 
-pub struct WriteStats<W>(::std::marker::PhantomData<W>);
-
+pub struct WriteStats<W> {
+    _marker: ::std::marker::PhantomData<W>,
+    writer: W,
+    num_writes: usize,
+    num_bytes_written: usize,
+}
 impl<W: Write> WriteStats<W> {
     // _wrapped is ignored because W is not bounded on Debug or Display and therefore
     // can't be passed through format!(). For actual implementation you will likely
     // wish to remove the leading underscore so the variable is not ignored.
-    pub fn new(_wrapped: W) -> WriteStats<W> {
-        WriteStats(PhantomData)
+    pub fn new(writer: W) -> WriteStats<W> {
+        WriteStats {
+            _marker: PhantomData,
+            writer,
+            num_writes: 0,
+            num_bytes_written: 0,
+        }
     }
 
     pub fn get_ref(&self) -> &W {
-        unimplemented!("WriteStats get_ref")
+        &self.writer
     }
 
     pub fn bytes_through(&self) -> usize {
-        unimplemented!("WriteStats bytes_through")
+        self.num_bytes_written
     }
 
     pub fn writes(&self) -> usize {
-        unimplemented!("WriteStats writes")
+        self.num_writes
     }
 }
 
 impl<W: Write> Write for WriteStats<W> {
     fn write(&mut self, buf: &[u8]) -> Result<usize> {
-        unimplemented!("Collect statistics about this call writing {:?}", buf)
+        let n = self.writer.write(buf)?;
+        self.num_writes += 1;
+        self.num_bytes_written += n;
+        Ok(n)
     }
 
     fn flush(&mut self) -> Result<()> {
-        unimplemented!("WriteStats write")
+        self.writer.flush()
     }
 }
