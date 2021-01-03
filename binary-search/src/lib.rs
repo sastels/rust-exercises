c258ca78d05c2a2189f4911c5d47fb9f1865a5f7
Steve Astels <sastels@gmail.com>
2021-01-02 20:15:25 -0500

    non-generic

--- binary-search/src/lib.rs
+++ binary-search/src/lib.rs
@@ -1,7 +1,21 @@
+use std::cmp::Ordering;
+
 pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    unimplemented!(
-        "Using the binary search algorithm, find the element '{}' in the array '{:?}' and return its index.",
-        key,
-        array
-    );
+    if array.is_empty() {
+        return None;
+    }
+    let middle_index = array.len() / 2;
+    let middle = array[middle_index];
+
+    match key.cmp(&middle) {
+        Ordering::Equal => Some(middle_index),
+        Ordering::Less => find(&array[..middle_index], key),
+        Ordering::Greater => {
+            if let Some(found) = find(&array[(middle_index + 1)..], key) {
+                Some(middle_index + 1 + found)
+            } else {
+                None
+            }
+        }
+    }
 }
