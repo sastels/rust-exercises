71a27f8b167cbc87ad5554c86d55d39a6ab73376
Steve Astels <sastels@gmail.com>
2021-01-02 20:45:37 -0500

    generic

--- binary-search/src/lib.rs
+++ binary-search/src/lib.rs
@@ -1,14 +1,20 @@
+use core::cmp::Ord;
 use std::cmp::Ordering;
 
-pub fn find(array: &[i32], key: i32) -> Option<usize> {
+pub fn find<U, T>(array: U, key: T) -> Option<usize>
+where
+    U: AsRef<[T]>,
+    T: Ord,
+{
+    let array = array.as_ref();
     if array.is_empty() {
         return None;
     }
     let middle_index = array.len() / 2;
-    let middle = array[middle_index];
+    let middle = &array[middle_index];
     let (left, right) = array.split_at(middle_index);
 
-    match key.cmp(&middle) {
+    match key.cmp(middle) {
         Ordering::Equal => Some(middle_index),
         Ordering::Less => find(left, key),
         Ordering::Greater => {
