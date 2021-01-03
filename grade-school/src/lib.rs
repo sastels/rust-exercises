1d6d2a0c1c119403e9901364af713d6b970d28b5
Steve Astels <sastels@gmail.com>
2021-01-02 19:48:26 -0500

    done

--- grade-school/src/lib.rs
+++ grade-school/src/lib.rs
@@ -1,16 +1,31 @@
-pub struct School {}
+use std::collections::HashMap;
+
+pub struct School {
+    enrolled: HashMap<u32, Vec<String>>,
+}
+
+impl Default for School {
+    fn default() -> Self {
+        Self::new()
+    }
+}
 
 impl School {
     pub fn new() -> School {
-        unimplemented!()
+        School {
+            enrolled: HashMap::new(),
+        }
     }
 
     pub fn add(&mut self, grade: u32, student: &str) {
-        unimplemented!("Add {} to the roster for {}", student, grade)
+        let kids = self.enrolled.entry(grade).or_insert_with(Vec::new);
+        kids.push(student.to_string());
     }
 
     pub fn grades(&self) -> Vec<u32> {
-        unimplemented!()
+        let mut grades: Vec<_> = self.enrolled.keys().copied().collect();
+        grades.sort();
+        grades
     }
 
     // If grade returned an `Option<&Vec<String>>`,
@@ -18,6 +33,14 @@ impl School {
     // By returning an owned vector instead,
     // the internal implementation is free to use whatever it chooses.
     pub fn grade(&self, grade: u32) -> Option<Vec<String>> {
-        unimplemented!("Return the list of students in {}", grade)
+        let gr = self.enrolled.get(&grade);
+        match gr {
+            Some(gr) => {
+                let mut gr = gr.clone();
+                gr.sort();
+                Some(gr)
+            }
+            None => None,
+        }
     }
 }
