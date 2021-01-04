c1f2ac33213564b1c1fe0ef212d83a1b0b8c4996
Steve Astels <sastels@gmail.com>
2021-01-04 15:15:14 -0500

    done

--- bowling/src/lib.rs
+++ bowling/src/lib.rs
@@ -4,18 +4,83 @@ pub enum Error {
     GameComplete,
 }
 
-pub struct BowlingGame {}
+pub struct BowlingGame {
+    rolls: Vec<u16>,
+    pins_left: u16,
+    frame: usize,
+    frame_ball: usize,
+    balls_per_frame: usize,
+}
 
 impl BowlingGame {
     pub fn new() -> Self {
-        unimplemented!();
+        BowlingGame {
+            rolls: vec![],
+            pins_left: 10,
+            frame: 1,
+            frame_ball: 1,
+            balls_per_frame: 2,
+        }
     }
 
     pub fn roll(&mut self, pins: u16) -> Result<(), Error> {
-        unimplemented!("Record that {} pins have been scored", pins);
+        if self.frame > 10 {
+            Err(Error::GameComplete)
+        } else if pins > self.pins_left {
+            Err(Error::NotEnoughPinsLeft)
+        } else {
+            self.rolls.push(pins);
+
+            if self.frame == 10 {
+                if pins == self.pins_left {
+                    if self.frame_ball < 3 {
+                        self.balls_per_frame = 3;
+                    }
+                    self.pins_left = 10;
+                } else {
+                    self.pins_left -= pins;
+                }
+                if self.frame_ball >= self.balls_per_frame {
+                    self.frame = 11;
+                }
+                self.frame_ball += 1;
+            } else {
+                // not frame 10
+                if pins == self.pins_left || self.frame_ball == 2 {
+                    self.frame += 1;
+                    self.pins_left = 10;
+                    self.frame_ball = 1;
+                } else {
+                    self.frame_ball += 1;
+                    self.pins_left -= pins;
+                }
+            }
+
+            Ok(())
+        }
     }
 
     pub fn score(&self) -> Option<u16> {
-        unimplemented!("Return the score if the game is complete, or None if not.");
+        if self.frame <= 10 {
+            None
+        } else {
+            let mut score = 0;
+            let mut ball = 0;
+            for _ in 0..10 {
+                let first_ball = self.rolls[ball];
+                let second_ball = self.rolls[ball + 1];
+                let mut third_ball = 0;
+                if first_ball == 10 || first_ball + second_ball == 10 {
+                    third_ball = self.rolls[ball + 2];
+                }
+                score += first_ball + second_ball + third_ball;
+                if first_ball == 10 {
+                    ball += 1
+                } else {
+                    ball += 2
+                }
+            }
+            Some(score)
+        }
     }
 }
