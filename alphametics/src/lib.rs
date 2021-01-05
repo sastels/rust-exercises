74fda87ae2ab88a73467375d7e49aac099e9867f
Steve Astels <sastels@gmail.com>
2021-01-05 12:08:36 -0500

    working I think but slowwww

--- alphametics/src/lib.rs
+++ alphametics/src/lib.rs
@@ -2,15 +2,31 @@ use itertools::Itertools;
 use std::collections::HashMap;
 
 fn check_eqn(input: &str, vals: &HashMap<char, u8>) -> bool {
+    // return false;
+
     let mut equation = input.to_string().replace(' ', "");
     for (key, val) in vals {
         equation = equation.replace(*key, &val.to_string());
     }
     let sum = equation.split("==").next().unwrap();
-    let answer: usize = equation.split("==").last().unwrap().parse().unwrap();
-    let sum: usize = sum.split('+').map(|x| x.parse::<usize>().unwrap()).sum();
-    println!("{:?}  sum: {} answer {}", &vals, sum, answer);
+    let answer: String = equation.split("==").last().unwrap().to_string();
+    if answer.starts_with('0') {
+        return false;
+    }
+    let answer: usize = answer.parse().unwrap();
+    let summands: Vec<String> = sum.split('+').map(|s| s.to_string()).collect();
+    if summands.iter().any(|s| s.starts_with('0')) {
+        return false;
+    }
 
+    let sum: usize = summands.iter().map(|x| x.parse::<usize>().unwrap()).sum();
+    if answer == 101 {
+        println!("{:?}  sum: {} answer {}", &vals, sum, answer);
+    }
+
+    if sum == answer {
+        println!("{:?}  sum: {} answer {} ****", &vals, sum, answer);
+    }
     sum == answer
 }
 
@@ -24,12 +40,18 @@ pub fn solve(input: &str) -> Option<HashMap<char, u8>> {
     println!("letters: {:?}", letters);
     let perms: Vec<_> = (0..10).permutations(letters.len()).collect();
 
-    let solutions = perms.iter().map(|v| {
-        let solns: HashMap<char, u8> = letters.iter().cloned().zip(v.iter().cloned()).collect();
-        return solns;
-    });
+    let solutions: Vec<HashMap<_, _>> = perms
+        .iter()
+        .map(|v| {
+            let solns: HashMap<char, u8> = letters.iter().cloned().zip(v.iter().cloned()).collect();
+            return solns;
+        })
+        .collect();
 
-    let solutions: Vec<_> = solutions.filter(|solns| check_eqn(input, solns)).collect();
+    let solutions: Vec<_> = solutions
+        .iter()
+        .filter(|solns| check_eqn(input, solns))
+        .collect();
 
     if !solutions.is_empty() {
         return Some(solutions[0].clone());
