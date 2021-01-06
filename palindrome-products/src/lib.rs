42293372013bb0b5c4964a48db6f27c3af87d428
Steve Astels <sastels@gmail.com>
2021-01-06 12:07:35 -0500

    done

--- palindrome-products/src/lib.rs
+++ palindrome-products/src/lib.rs
@@ -1,26 +1,53 @@
 #[derive(Debug, PartialEq, Eq)]
 pub struct Palindrome {
-    // implement your palindrome type here
+    factors: Vec<(u64, u64)>,
 }
 
 impl Palindrome {
     pub fn new(a: u64, b: u64) -> Palindrome {
-        unimplemented!("create a palindrome with factors ({}, {})", a, b)
+        Palindrome {
+            factors: vec![(a, b)],
+        }
     }
 
     pub fn value(&self) -> u64 {
-        unimplemented!("return the value of this palindrome")
+        self.factors[0].0 * self.factors[0].1
     }
 
     pub fn insert(&mut self, a: u64, b: u64) {
-        unimplemented!("insert new factors ({}, {}) into this palindrome", a, b)
+        self.factors.push((a, b));
     }
 }
 
+fn is_palindrome(a: u64, b: u64) -> bool {
+    let product = format!("{}", a * b);
+    product == product.chars().rev().collect::<String>()
+}
+
 pub fn palindrome_products(min: u64, max: u64) -> Option<(Palindrome, Palindrome)> {
-    unimplemented!(
-        "Find the min and max palindromic numbers which are products of numbers in the inclusive range ({}..{})",
-        min,
-        max
-    )
+    let mut min_palindrome = Palindrome::new(max + 1, max + 1);
+    let mut max_palindrome = Palindrome::new(0, 0);
+
+    for a in min..=max {
+        for b in a..=max {
+            if is_palindrome(a, b) {
+                if a * b < min_palindrome.value() {
+                    min_palindrome = Palindrome::new(a, b);
+                } else if a * b == min_palindrome.value() {
+                    min_palindrome.insert(a, b);
+                }
+                if a * b > max_palindrome.value() {
+                    max_palindrome = Palindrome::new(a, b);
+                } else if a * b == max_palindrome.value() {
+                    max_palindrome.insert(a, b);
+                }
+            }
+        }
+    }
+
+    if max_palindrome.value() == 0 {
+        None
+    } else {
+        Some((min_palindrome, max_palindrome))
+    }
 }
